e97b9c1826f4240a59ced280a159db2cf3409c01
DTAlemayehu01 <daniel.alemayehu@colorado.edu>
2025-03-01 16:13:10 -0700

    Added PrivyProvider wrapper

--- Club Coin/src/main.tsx
+++ Club Coin/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {PrivyProvider} from '@privy-io/react-auth';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrivyProvider
+      appId="cm7qn7t0h00j4ybsvx75rn9rv"
+      config={{
+        // Display email and wallet as login methods
+        loginMethods: ['email', 'wallet'],
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      <App />
+    </PrivyProvider>
   </StrictMode>,
 )
